94c67aa8d6db8f15efd4c42a0ebf08a6b8f8baca
Thurzó Bence <thurzob@kkszki.hu>
2024-03-15 17:19:15 +0100

    reszponzivitás megvan a product oldalon

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -20,7 +20,7 @@ const ProductContent = styled.div`
   @media (max-width: 992px) {
     grid-template-columns: 1fr;
     margin-top: 5px;
-    margin-left: 0px;
+    margin-left: 25px;
     margin-right: auto;
     max-width: 90%;
     border-left: none;
@@ -59,7 +59,6 @@ top: 19%;
 width: 100%;
 
 @media (max-width: 992px) {
-  background-color: lightblue;
   justify-content: center;
   text-align: center;
 }
@@ -112,12 +111,51 @@ function Products() {
     const { addToCart,cartItems, setCartItems } = useCartContext();
     const { isLoggedIn, logout } = useContext(AuthContext);
     const [mappedData, setMappedData] = useState([]);
+    const [allmappedData, setAllMappedData] = useState([]);
     const [selectedProductId, setSelectedProductId] = useState(null);
     const [quantity, setQuantity] = useState(1);
     const [token, setToken] = useState('');
     const [userId, setUserId] = useState(''); 
     const [addedToCart, setAddedToCart] = useState(false); // Új állapot az ellenőrzéshez
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [allProducts, setAllProducts] = useState([]);
+    const increaseQuantity = () => {
+        setQuantity((prevQuantity) => prevQuantity + 1);
+    };
 
+    const decreaseQuantity = () => {
+        if (quantity > 1) {
+            setQuantity((prevQuantity) => prevQuantity - 1);
+        }
+    };
+    const [formWidth, setFormWidth] = useState('100%');
+    
+
+    const loadAllAutomatics = () => {
+        fetch('https://localhost:7276/api/Product', {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Accept': 'application/json',
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Az összes termékadat feldolgozása
+            setAllMappedData(data);
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+            setAllMappedData([]); // Ha hiba történik, üres tömbre állítjuk
+        });
+        setMappedData([]);
+    };
+    
     useEffect(() => {
         const savedCartItems = localStorage.getItem('cartItems');
         if (savedCartItems) {
@@ -140,13 +178,12 @@ function Products() {
     const handleProductSelect = (selectedProduct) => {
         setSelectedProductId(selectedProduct);
         setQuantity(1);
-        setMappedData([]);
         setAddedToCart(false); // Reset addedToCart állapot
-
+    
         if (!selectedProduct || selectedProduct === '') {
             return;
         }
-
+    
         fetch(`https://localhost:7276/api/Product/${selectedProduct}`, {
             method: 'GET',
             mode: 'cors',
@@ -165,18 +202,24 @@ function Products() {
                 products: [{
                     type: data.type,
                     serialName: data.serialName,
-                    price: data.price
+                    price: data.price,
+                     // Hozzáadott imageUrl mező
                 }]
             };
             setMappedData([mappedData]);
+            console.log(mappedData) // Minden esetben beállítjuk a mappedData-t
         })
         .catch(error => {
             console.error('Fetch error:', error);
             setMappedData([]);
         });
+    
+        // Minden más terméket töröljünk ki az allmappedData állapotból
+        setAllMappedData([]);
     };
 
-    const handleAddToCart = () => {
+    
+    const handleAddToCart = (quantity) => {
         if (selectedProductId && quantity > 0 && mappedData.length > 0 && !addedToCart) {
             const product = mappedData[0]?.products[0]; // Az első termék elérése
             if (product) {
@@ -189,17 +232,18 @@ function Products() {
                 // Ellenőrzi, hogy a termék már benne van-e a kosárban
                 if (alreadyInCartIndex !== -1) {
                     const updatedCartItems = [...cartItems];
-                    updatedCartItems[alreadyInCartIndex].quantity += quantity; // Csak a mennyiséget növeljük meg
+                    // Megkeressük a terméket a kosárban és növeljük a mennyiségét
+                    updatedCartItems[alreadyInCartIndex].quantity += quantity;
                     setCartItems(updatedCartItems);
                     localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
-                    setAddedToCart(true); // Állapot beállítása a további duplikáció megelőzésére
+                     // Állapot beállítása a további duplikáció megelőzésére
                 } else {
                     // Ha még nem szerepel a termék a kosárban, hozzáadjuk
                     const updatedCartItems = [
                         ...cartItems,
                         {
                             productId: parseInt(selectedProductId),
-                            quantity,
+                            quantity, // Itt használjuk a quantity változót
                             price: price || 0,
                             type: type || '',
                             serialName: serialName || '',
@@ -207,11 +251,48 @@ function Products() {
                     ];
                     setCartItems(updatedCartItems);
                     localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
-                    setAddedToCart(true); // Állapot beállítása a további duplikáció megelőzésére
+                    setAddedToCart(false); // Állapot beállítása a további duplikáció megelőzésére
                 }
             }
         }
     };
+
+    const handleAddToCartAll = (product, quantity) => {
+        if (product && quantity > 0 && !addedToCart) {
+            const { id, type, serialName, price } = product;
+    
+            const alreadyInCartIndex = cartItems.findIndex(
+                (item) => item.productId === parseInt(id)
+            );
+    
+            // Ellenőrzi, hogy a termék már benne van-e a kosárban
+            if (alreadyInCartIndex !== -1) {
+                const updatedCartItems = [...cartItems];
+                // Megkeressük a terméket a kosárban és növeljük a mennyiségét
+                updatedCartItems[alreadyInCartIndex].quantity += quantity;
+                setCartItems(updatedCartItems);
+                localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+                setAddedToCart(false); // Állapot visszaállítása
+            } else {
+                // Ha még nem szerepel a termék a kosárban, hozzáadjuk
+                const updatedCartItems = [
+                    ...cartItems,
+                    {
+                        productId: parseInt(id),
+                        quantity, // Itt használjuk a quantity változót
+                        price: price || 0,
+                        type: type || '',
+                        serialName: serialName || '',
+                    },
+                ];
+                setCartItems(updatedCartItems);
+                localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+                setAddedToCart(false); // Állapot beállítása a további duplikáció megelőzésére
+            }
+        }
+    };
+
+   
     
     const handleLogout = () =>{
         logout();
@@ -262,62 +343,96 @@ function Products() {
                 </Navbar>
 
 
-                             
+                
                 <div>               
                 <ResponsiveContainer> 
                 <div className="products-form" style={{borderLeft: '5px solid black' }}>
                 
-                
-                <h3 className='margin' style={{ marginTop: '15px', textDecoration: 'underline', border: 'none', fontSize: '36px' }}>
-                    Termékek
-                </h3>
-                
                 <div>
-                    <HorizontalRule />
-                </div>
-                <div className='margin'> 
-                    <h3 style={{ marginTop: '10px', textDecoration: 'underline' }}>Öntözőautomatikák</h3>
+                    {/* Új Navbar az űrlap felett */}
+                    <Navbar expand="lg" variant="dark" style={{borderRadius: '45px 50px 0 0'}}>                        
+                        <Navbar.Toggle style={{marginLeft: '25px'}} aria-controls="basic-navbar-nav" />                  
+                        <Navbar.Collapse style={{marginLeft: '25px'}} id="basic-navbar-nav">
+                        <Navbar.Brand style={{color:'bisque' ,fontSize: '17px'}} className="ml-2">Termékek:</Navbar.Brand>
+                            <Nav className="mr-auto">
+                                <Nav.Link style={{color:'bisque'}} as={NavLink} to="" onClick={loadAllAutomatics}>Öntözőautomatikák</Nav.Link>
+                                <Nav.Link style={{color:'bisque'}} as={NavLink} to="">Öntözőalkatrészek</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div> 
+                              
+ 
+                
+                
+                
+                <div  style={{marginTop: '30px',marginLeft: '25px', marginBottom: '30px'}}> 
+                    
                     <select onChange={(e) => handleProductSelect(e.target.value)}>
                         <option value="">Válassz egy Terméket</option>
-                        <optgroup label="HUNTER">
+                        <optgroup label="HUNTER Öntözőautomatikák">
                             <option value="1">HUNTER NODE-BT</option>
                             <option value="2">HUNTER X-CORE</option>
                         </optgroup>
+                        <optgroup label="HUNTER Öntözőalkatrészek">
+                            <option value="3">HUNTER Rain-Clik</option>
+                            <option value="4">HUNTER MP Rotator</option>
+                        </optgroup>
                     </select>
                 </div>
-                    {mappedData.length > 0 && (
-                        <ProductContent>
-                            {mappedData.map((item, index) => (
-                                <div key={index}>
-                                    {item.products.map((product, idx) => (
-                                        <div key={idx}>
-                                            <div style={{ float: 'right'}}>
-                                            {selectedProductId === '1' && (
-                                                <img src='https://res.cloudinary.com/dbmrgdyft/image/upload/v1707668947/HunterNode_tn2cla.jpg' alt='NODE-BT' style={{ width: '100%', maxWidth: '300px' }} />
-                                            )}
-                                            <div>
-                                            {selectedProductId === '2' && (
-                                                <img src='https://res.cloudinary.com/dbmrgdyft/image/upload/v1707667618/HunterXcore_rwnbta.jpg' alt='X-CORE' style={{ width: '100%', maxWidth: '300px' }} />
-                                            )}
-                                            </div>
-                                            </div>
-                                            <div>
-                                                <p style={{ fontSize: '25px', fontWeight: 'bold' }}>Termék típusa: {product.type}</p>
-                                                <p style={{ fontSize: '20px', fontWeight: 'bold' }}>Széria név: {product.serialName}</p>
-                                                <p style={{ fontSize: '20px', fontWeight: 'bold', textDecoration: 'underline' }}>Ár: {product.price} Ft.</p>
-                                            </div>
-                                            
-                                            <div style={{float: 'left', textAlign: 'center', marginTop: '20px' }}>
-                                                <input type="number" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} style={{ width: '60px' }} />
-                                                <button onClick={handleAddToCart} style={{ marginLeft: '10px' }}>Kosárba</button>
-                                            </div>
-                                        </div>
-                                    ))}
+                {mappedData.length > 0 && (
+                    <ProductContent >
+                        {mappedData[0]?.products && mappedData[0].products.map((product, index) => (
+                            <div key={index}>
+                                <div style={{ float: 'left', marginRight: '20px' }}>
+                                    {selectedProductId === '1' && (
+                                        <img src='https://res.cloudinary.com/dbmrgdyft/image/upload/v1707668947/HunterNode_tn2cla.jpg' alt='NODE-BT' style={{ width: '100%', maxWidth: '150px' }} />
+                                    )}
+                                    {selectedProductId === '2' && (
+                                        <img src='https://res.cloudinary.com/dbmrgdyft/image/upload/v1707667618/HunterXcore_rwnbta.jpg' alt='X-CORE' style={{ width: '100%', maxWidth: '150px' }} />
+                                    )}
                                 </div>
-                            ))}
-                        </ProductContent>
-                    )}
-                    
+                                <div style={{ overflow: 'hidden' }}>
+                                    <p style={{ fontSize: '14px', fontWeight: 'bold', marginBottom: '5px' }}>Termék típusa: {product.type}</p>
+                                    <p style={{ fontSize: '12px', marginBottom: '5px' }}>Széria név: {product.serialName}</p>
+                                    <p style={{ fontSize: '12px', marginBottom: '5px' }}>Ár: {product.price} Ft.</p>
+                                    <button onClick={decreaseQuantity}>-</button>
+                                    <span style={{ margin: '0 10px' }}>{quantity}</span>
+                                    <button onClick={increaseQuantity}>+</button>
+                                    <button onClick={() => handleAddToCart(quantity)}>Kosárba</button>
+                                </div>
+                            </div>
+                        ))}
+                    </ProductContent>
+                )}
+
+                {allmappedData.length > 0 &&  (
+                    <ProductContent>
+                        {allmappedData.map((product, index) => (
+                            <div key={index}>
+                                <div style={{ float: 'left', marginRight: '20px' }}>
+                                {product.id == 1 && (
+                                        <img src='https://res.cloudinary.com/dbmrgdyft/image/upload/v1707668947/HunterNode_tn2cla.jpg' alt='NODE-BT' style={{ width: '100%', maxWidth: '150px' }} />
+                                    )}
+                                {product.id == 2 && (
+                                        <img src='https://res.cloudinary.com/dbmrgdyft/image/upload/v1707667618/HunterXcore_rwnbta.jpg' alt='NODE-BT' style={{ width: '100%', maxWidth: '150px' }} />
+                                    )}    
+                                    
+                                </div>
+                                <div style={{ overflow: 'hidden' }}>
+                                    
+                                    <p style={{ fontSize: '14px', fontWeight: 'bold', marginBottom: '5px' }}>Termék típusa: {product.type}</p>
+                                    <p style={{ fontSize: '12px', marginBottom: '5px' }}>Széria név: {product.serialName}</p>
+                                    <p style={{ fontSize: '12px', marginBottom: '5px' }}>Ár: {product.price} Ft.</p>
+                                    <button onClick={decreaseQuantity}>-</button>
+                                    <span style={{ margin: '0 10px' }}>{quantity}</span>
+                                    <button onClick={increaseQuantity}>+</button>
+                                    <button onClick={() => handleAddToCartAll(product ,quantity)}>Kosárba</button>
+                                </div>
+                            </div>
+                        ))}
+                    </ProductContent>
+                )}
                 
                             
             </div>
