2905dc6bde535f7ece33f2d4c2b3c99dd8ee0789
Thurzó Bence <thurzob@kkszki.hu>
2024-04-17 15:53:14 +0200

    Már csak szépíteni kell a frontenden

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -209,10 +209,7 @@ const handleSendPasswordReset = async () => {
       
   })
       .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          return response.json();
+          
       })
       .then(data => {
           console.log(data);
